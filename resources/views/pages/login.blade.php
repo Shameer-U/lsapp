f383ce265375da870a72b6bea2a362571adee61f
Shameer-U <shameerushahul@gmail.com>
2020-08-03 00:44:36 +0530

    Update login.blade.php

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -6,7 +6,7 @@
 <div class="row">
     <div class="offset-md-4 col-md-4">
         <h2 class="text-center">Login</h2>
-        <form class="p-4"  action="{{ url('users/login') }}" method="POST" > <!--"posts" points to "PostsController@store" ( to view use "php artisan route:list") -->
+        <form class="p-4"  action="{{ url('users/login') }}" method="POST" > 
             {{--  it is always good to use url helper  {{ url('') }} --}}
             {{ csrf_field() }} {{-- without csrf field form wont submit --}}
                 <div class="form-group">
